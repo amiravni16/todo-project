b667fae1abbf062bb35329ffe6a59a15a6eddccc
amiravni16 <amiravni13@gmail.com>
2025-08-20 16:13:38 +0300

    Apply redux to app and create import to store

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import { RootCmp } from './RootCmp.jsx'
+import { store } from './store/store.js'
 
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
-root.render(<RootCmp />)
+root.render(
+    <ReactRedux.Provider store={store}>
+        <RootCmp />
+    </ReactRedux.Provider>
+)
